3104063f647e3092297a413b46a73788e6f29595
Pavel Ilin <pavel_ilin@yahoo.com>
2019-08-30 22:45:33 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,6 +8,6 @@ def map_to_no_change(source_array)
     source_array
 end
 
-def map_to_negativize(source_array)
-   source_array.map() {|i| i * -1}
+def map_to_double(source_array)
+   source_array.map() {|i| i * 2}
 end
