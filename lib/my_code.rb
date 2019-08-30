dd8f1e3b66104ac499c474f8f056edc77dbaecf4
Pavel Ilin <pavel_ilin@yahoo.com>
2019-08-30 22:46:21 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,3 +15,6 @@ end
 def map_to_square(source_array)
    source_array.map() {|i| i ** 2}
 end
+
+def reduce_to_total(sourceArray, startingPoint = 0)
+    sourceArray.reduce(startingPoint) { |total, num| total + num}
